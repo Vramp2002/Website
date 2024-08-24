afb801b92a08a2485e541d2e1f6cde8da454735a
Ram Prakash V <161312866+Vramp2002@users.noreply.github.com>
2024-08-24 22:13:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+// const navToggle = document.querySelector('.nav__toggle');
+//   const nav = document.querySelector('nav');
+//
+//   navToggle.on('click', () => {
+//     console.log("Button Clicked");
+//     nav.classList.toggle('active');
+//   });
+
+
+function toggleMenu() {
+  const nav = document.querySelector('nav');
+  console.log("Clicked");
+  nav.classList.toggle('active');
+}
